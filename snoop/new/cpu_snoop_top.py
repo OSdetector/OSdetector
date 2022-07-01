a6ebcc047adf14943d036b7a12757d26b5053a2a
li <1351855206@qq.com>
2022-07-01 19:08:37 +0800

    增加对2种不基于ebpf的CPU占用监控模块的多进程支持

--- snoop/new/cpu_snoop_top.py
+++ snoop/new/cpu_snoop_top.py
@@ -1,28 +1,35 @@
 import os
+from utils import bfs_get_procs
 
 def cpu_top_record(output_file, cur_time, snoop_pid):
-    utilization, comm = float(os.popen('top -bi -n 2 -d 0.02 -p '+str(snoop_pid)).read().split('\n')[7].split()[-4]), os.popen('top -bi -n 2 -d 0.02 -p '+str(snoop_pid)).read().split('\n')[7].split()[-1]
-    if not output_file is None:
-        output_file.write("%.2f,%12d,%20s,%d,%d,%.2f\n" % (
-                        cur_time,
-                        snoop_pid, 
-                        comm,
-                        -1, 
-                        -1,
-                        utilization,
-                        )
-        )
-        output_file.flush()
-    else:
-        print("%.2f,%12d,%20s,%d,%d,%.2f\n" % (
-                        cur_time,
-                        snoop_pid, 
-                        comm,
-                        -1, 
-                        -1,
-                        utilization,
-                        )
-        )
+    pid_list = bfs_get_procs(snoop_pid)
+    for pid in pid_list:
+        try:
+            row = os.popen('top -b -n 2 -d 0.02 -p '+str(pid)).read().split('\n')[7].split()
+            utilization, comm = float(row[-4]), row[-1]
+            if not output_file is None:
+                output_file.write("%.2f,%12d,%20s,%d,%d,%.2f\n" % (
+                                cur_time,
+                                pid, 
+                                comm,
+                                -1, 
+                                -1,
+                                utilization,
+                                )
+                )
+                output_file.flush()
+            else:
+                print("%.2f,%12d,%20s,%d,%d,%.2f\n" % (
+                                cur_time,
+                                pid, 
+                                comm,
+                                -1, 
+                                -1,
+                                utilization,
+                                )
+                )
+        except IndexError:
+            continue
     
 
 if __name__=="__main__":
